b7455b83ba024c23b0c899a7f03c4cfab45bd744
Nikolay Garbuziuk <n_g_@ukr.net>
2022-02-16 20:07:02 +0200

    added tests with allure log examples

---
+++ src/test/java/tests/BookStoreTests.java
@@ -0,0 +1,133 @@
+package tests;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import org.junit.jupiter.api.Test;
+
+import static filters.CustomLogFilter.customLogFilter;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class BookStoreTests {
+
+    // https://demoqa.com/swagger/#/
+    // BookStore/v1/Books
+
+    @Test
+    void noLogsTest() {
+        given()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void allLogsTest() {
+        given()
+                .log().all()
+                .when()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .log().all()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void someLogsTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void authorizeTest() {
+        String data = "{" +
+                "  \"userName\": \"alex\"," +
+                "  \"password\": \"asdsad#frew_DFS2\"" +
+                "}";
+
+        given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(data)
+                .when()
+                .log().uri()
+                .log().body()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+
+    @Test
+    void authorizeWithListenerTest() {
+        String data = "{" +
+                "  \"userName\": \"alex\"," +
+                "  \"password\": \"asdsad#frew_DFS2\"" +
+                "}";
+
+        given()
+                .filter(new AllureRestAssured())
+                .contentType("application/json")
+                .accept("application/json")
+                .body(data)
+                .when()
+                .log().uri()
+                .log().body()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+
+    @Test
+    void authorizeWithTemplatesTest() {
+        String data = "{" +
+                "  \"userName\": \"alex\"," +
+                "  \"password\": \"asdsad#frew_DFS2\"" +
+                "}";
+
+        given()
+                .filter(customLogFilter().withCustomTemplates())
+                .contentType("application/json")
+                .accept("application/json")
+                .body(data)
+                .when()
+                .log().uri()
+                .log().body()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+
+    @Test
+    void authorizeWithSchemaTest() {
+        String data = "{" +
+                "  \"userName\": \"alex\"," +
+                "  \"password\": \"asdsad#frew_DFS2\"" +
+                "}";
+
+        given()
+                .filter(new AllureRestAssured())
+                .contentType("application/json")
+                .accept("application/json")
+                .body(data)
+                .when()
+                .log().uri()
+                .log().body()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body(matchesJsonSchemaInClasspath("schemas/GenerateTokenSchema.json"))
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+}
